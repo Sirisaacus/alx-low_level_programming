c13c6b14da06472300d064f559f6d6754db9fc6f
Sirisaacus <sirisaacus@gmail.com>
2022-09-12 02:59:53 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all letters of the alphabet
+ * except q and e
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char ch = 'a';
+
+  for (; ch <= 'z'; ch++)
+    {
+      if (ch != 'q' && ch != 'e')
+	putchar(ch);
+    }
+
+  return (0);
+}
