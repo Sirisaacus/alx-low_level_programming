a63fed2c6c6d39c7884b4811f25163dbc88e7b87
Sirisaacus <sirisaacus@gmail.com>
2022-09-13 19:03:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-lines (11 sloc)  252 Bytes
 #include <stdio.h>
 #include <unistd.h>
 
@@ -9,6 +8,6 @@ lines (11 sloc)  252 Bytes
 
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
