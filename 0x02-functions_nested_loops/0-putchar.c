d71fd84a8c2105b4842c81a6780fea511b611c7c
Sirisaacus <sirisaacus@gmail.com>
2022-09-15 13:40:27 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point prints "_putchar"
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	char a[] = "_putchar";
+	int j;
+
+	for (j = 0; j < 8; j++)
+	{
+		_putchar(a[j]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
