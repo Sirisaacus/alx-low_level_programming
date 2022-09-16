b39efb213d6e209c5a630ae1502026065eeab37f
Sirisaacus <sirisaacus@gmail.com>
2022-09-16 12:23:05 -0700

    .

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+unsigned long i, a = 1, b = 2, c;
+
+printf("%lu, %lu, ", a, b);
+
+for (i = 0; i < 48; i++)
+{
+c = a + b;
+if (i == 47)
+printf("%lu", c);
+else
+printf("%lu, ", c);
+a = b;
+b = c;
+}
+
+printf("\n");
+
+return (0);
+}
