cd6ddf36cc1c151daedc847966df540b433f9b0d
Sirisaacus <sirisaacus@gmail.com>
2022-09-28 06:33:04 -0700

    .

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* len_num - count string length
+* @str: pointer to string
+*
+* Return: Always 0
+*/
+
+int len_num(char *str)
+{
+if (*str == '\0')
+return (0);
+else
+return (1 + len_num(str + 1));
+}
+
+/**
+* _strlen_recursion - a function that returns
+* the length of string
+* @s: pointer to string
+*
+* Return: Always 0
+*/
+
+int _strlen_recursion(char *s)
+{
+int len = 0;
+
+len = len_num(s);
+return (len);
+}
