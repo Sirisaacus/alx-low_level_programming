d656532e8d0d45a76520f6d0bfe0db2e675fbc0d
Sirisaacus <sirisaacus@gmail.com>
2022-10-27 03:43:32 -0700

    .

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* binary_to_uint - converts a binary number to unsigned int
+* @b: pointer to number
+* Return: converted number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int i, res = 0;
+
+if (!b)
+return (0);
+
+
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+res = 2 * res + (b[i] - '0');
+}
+return (res);
+}
